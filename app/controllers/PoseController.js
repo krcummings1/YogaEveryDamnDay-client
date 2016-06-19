1f158a7eddcc4d2a42a407dd624e2ca4e398c343
Kaylee <kayleercummings@gmail.com>
2016-06-18 20:48:29 -0500

    getting data back in the console, but not with supported poses

--- app/controllers/PoseController.js
+++ app/controllers/PoseController.js
@@ -1,23 +1,3 @@
-// "use strict";
-
-// YEDD.controller('PoseController',
-//   [
-//     "$scope",
-//     "$PoseFactory",
-//     "$route",
-
-//     function ($scope, $PoseFactory, $route) {
-
-//       $scope.allPoses = PoseFactory();
-//       $scope.pose = {};
-//       console.log("hello there");
-//       console.log("all poses", allPoses);
-
-
-//     }
-
-//   ]);
-
 "use strict";
 
 YEDD.controller('PoseController', [
@@ -26,27 +6,14 @@ YEDD.controller('PoseController', [
 
   function ($scope, $http) {
 
-    // $scope.poses = [];
-    $scope.data = [];
+    // $scope.figurines = [];
+    var data;
 
     $http
-      .get('http://localhost:5000/api/Pose/')
-      .success(data);
-
-      console.log("past the GET");
-      // console.log("poses", $scope.poses);
-      console.log("data", data);
-
-    // $scope.deleteToy = function (id) {
-    //   $http({
-    //     method: "DELETE",
-    //     url: `http://localhost:5000/api/Inventory/${id}`
-    //   })
-    //   .then(
-    //     () => console.log("Toy deleted"),
-    //     () => console.log("Toy not deleted")
-    //   );
-    // }
+      .get('http://localhost:5000/api/Pose')
+      .success(data => console.log("data", data));
+      // .success(inv => $scope.figurines = inv);
+      
 
   }
 
