c741e1d7b5f1a61c0f5dc8a8a2654f3c6e02154d
Kaylee <kayleercummings@gmail.com>
2016-06-24 01:39:31 -0500

    main page is styled

--- app/controllers/HeaderController.js
+++ app/controllers/HeaderController.js
@@ -19,5 +19,17 @@ YEDD.controller('HeaderController',
         $route.reload();
     });
 
+
+    function collapseNavbar() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    }
+
+    $(window).scroll(collapseNavbar);
+    $(document).ready(collapseNavbar);
+
   } // closes main function
 ]);
